3ae06ddd0a75d2804277b0a9bddb71a9333886af
Rob Peace <rob.peace@gmail.com>
2015-04-15 13:11:06 -0400

    Fasta stats now include standard deviations

--- fasta_stats.py
+++ fasta_stats.py
@@ -64,7 +64,10 @@ def getGCratios(seqs):
 		seqLen = float(len(seq))
 		seqGC = (float(seq.count('G')) + float(seq.count('C'))) / seqLen
 		seqG = float(seq.count('G')) / seqLen
-		ratios.append(seqG / seqGC)
+		if seqGC == 0:
+			ratios.append(0.0)
+		else:
+			ratios.append(seqG / seqGC)
 	return ratios
 
 
@@ -98,29 +101,33 @@ for idx, seq in enumerate(seqs):
 	outFile.write(str(GGCs[idx])+',')
 	outFile.write(str(AAUs[idx])+'\n')
 outFile.write("Header,Sequence,Length,\"GC content\",\"GC content (2-8)\",G/GC,A/AU\n")
-outFile.write(",Average,")
+outFile.write(",Mean,")
 outFile.write(str(sum(lens)/len(lens))+',')
 outFile.write(str(sum(GCs)/len(GCs))+',')
 outFile.write(str(sum(GC28s)/len(GC28s))+',')
 outFile.write(str(sum(GGCs)/len(GGCs))+',')
 outFile.write(str(sum(AAUs)/len(AAUs))+'\n')
+outFile.write(",Std,")
+outFile.write(str(np.std(np.array(lens)))+',')
+outFile.write(str(np.std(np.array(GCs)))+',')
+outFile.write(str(np.std(np.array(GC28s)))+',')
+outFile.write(str(np.std(np.array(GGCs)))+',')
+outFile.write(str(np.std(np.array(AAUs)))+'\n')
 outFile.close()
 
-# GCmean = sum(GCs)/len(GCs)
-# otherGCs = getGCs("Physarum_miRNA.seqs")
-# otherMean = sum(otherGCs)/len(otherGCs)
-# print GCmean
+# arr = np.array(GC28s)
+
+# print "mean:", np.mean(arr)
+# print "std:", np.std(arr)
 
 # (mu, sigma) = norm.fit(GCs)
 # print "Mu:", mu
 # print "Sigma:", sigma
 
-# n, bins, patches = plt.hist(GCs, normed=1, bins=50, color='c')
+# n, bins, patches = plt.hist(GC28s, normed=1, bins=50, color='c')
 # # plt.hist(otherGCs, normed=1, bins=5, color='r', alpha = 0.5)
 # y = mlab.normpdf(bins, mu, sigma)
 # l = plt.plot(bins, y, 'b--', linewidth=2)
-# plt.axvline(GCmean, color='b', linestyle='dashed', linewidth=2)
-# for pt in otherGCs:
-# 	plt.axvline(pt, color='g', linewidth=1)
+# plt.axvline(np.mean(arr), color='b', linestyle='dashed', linewidth=2)
 # plt.show()
 
